5aaeb2300ff6cd9352ed6adc0edeb9290f42e8d4
MarceloMederi <marcelomederi11@gmail.com>
2023-10-16 22:56:07 -0300

    Configurado a porta do servidor server.js

--- server.js
+++ server.js
@@ -9,29 +9,25 @@ app.use(express.json());
 
 const dadosFilePath = path.join(__dirname, 'dados.json');
 
-// Configuração para servir arquivos estáticos
 app.use(express.static(__dirname));
 
 app.post('/salvar-dados', (req, res) => {
     const formData = req.body;
-
-    // Lê os dados do arquivo
     let dados = [];
     try {
         const data = fs.readFileSync(dadosFilePath, 'utf8');
         dados = JSON.parse(data);
     } catch (error) {
-        // O arquivo não existe ou ocorreu um erro na leitura, trata como um array vazio
         dados = [];
     }
-
-    // Adiciona os novos dados ao final da lista
     dados.push(formData);
-
-    // Salva os dados no arquivo
-    fs.writeFileSync(dadosFilePath, JSON.stringify(dados), 'utf8');
-
-    res.send('Dados salvos com sucesso.');
+    try {
+        fs.writeFileSync(dadosFilePath, JSON.stringify(dados), 'utf8');
+        res.send('Dados salvos com sucesso.');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao salvar os dados.');
+    }
 });
 
 app.listen(port, () => {
